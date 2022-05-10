099ab949d3bf4834c7952a8345a16b28d196bdbc
aibauer <aibauer7@gmail.com>
2022-05-10 17:00:27 -0400

    UI changes and file upload

--- src/Components/pages/homePage.js
+++ src/Components/pages/homePage.js
@@ -13,7 +13,7 @@ import resume from './files/Resume.pdf';
                 <img src={image} alt="profile" />
                 <h1>Andrew Bauer</h1>
                 <p>
-                    Enterprise Transformation Intern at Grant Thornton | Computer Science Student at Georgia Tech
+                    Associate Product Intern at American Express | Computer Science Student at Georgia Tech
                 </p>
             </div>
             <div className="contact">
