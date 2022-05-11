f7b3bd335f8c1e0912e094808d66ab2bc0ed7e4a
aibauer <aibauer7@gmail.com>
2022-05-11 11:03:37 -0400

    layout changes to projects page and other revisions

--- src/Components/pages/projectsPage.js
+++ src/Components/pages/projectsPage.js
@@ -17,44 +17,46 @@ import proj3 from "./files/ruok.jpg";
             </h1>
           </div>
           <div className ="projContainer">
-            <a href="http://wildlife-tagger.herokuapp.com/" target="_blank" color="#235789">
+            <p></p>
+            <h1>ATL Wildin'</h1>
+            <p></p>
+            <a href="http://wildlife-tagger.herokuapp.com/" target="_blank">
             <img src = {proj1} alt="vis" />
-            <div>
-              <p></p>
-              <a>ATL Wildin'</a>
-              <p></p>
+            </a>
+            <div class="content">
               <p>Biodiversity surveying game where users tag presented photos of animals as a means of data collection of biodiveristy on Georgia Tech's campus greenspaces</p>
               <p>Developed as a Django web application, using mySQL databases for the animal and image data, with a python backend and an HTML, CSS, and javascript front end</p>
               <p>Completed as junior design project at Georgia Tech</p>
             </div>
-            </a>
+            
           </div>
           <div className ="projContainer">
-            <a href="https://dwwilson3.github.io/Brain-Tumor-Classification/report_final" target="_blank" color="#235789">
+            <p></p>
+            <h1>Brain Tumor Classification</h1>
+            <p></p>
+            <a href="https://dwwilson3.github.io/Brain-Tumor-Classification/report_final" target="_blank">
             <img src = {proj2} alt="bs" />
-            <div>
-              <p></p>
-              <a>Brain Tumor Classification</a>
-              <p></p>
-              <p>Group project for machine learning course</p>
-              <p>Sought to create machine learning model to classify dataset of brain MRI images based on type of tumor presented</p>
-              <p>Utilized python packges such as numpy, pandas, matplotlib, keras, tensorflow, and sklearn to perform kmeans clustering and creater neural network and cnn on MRI brain scan dataset</p>
-              
-            </div>
             </a>
+            <div class="content">
+              <p>Machine learning model to classify dataset of brain MRI images based on type of tumor presented</p>
+              <p>Utilized python packges such as numpy, pandas, matplotlib, keras, tensorflow, and sklearn to perform kmeans clustering and create neural network and cnn to classify Brain MRI Image dataset</p>
+              <p>Group project for machine learning course</p>
+            </div>
+            
           </div>
           <div className ="projContainer">
-            <a href="https://www.figma.com/proto/7hbTemmD0qs0wpMmp6togn/CS-3873?node-id=145%3A261&scaling=min-zoom&page-id=145%3A2&starting-point-node-id=145%3A261" target="_blank" color="#235789">
+            <p></p>
+            <h1>R U OK?</h1>
+            <p></p>
+            <a href="https://www.figma.com/proto/7hbTemmD0qs0wpMmp6togn/CS-3873?node-id=145%3A261&scaling=min-zoom&page-id=145%3A2&starting-point-node-id=145%3A261" target="_blank">
             <img src = {proj3} alt="ruok" />
-            <div>
-              <p></p>
-              <a>R U OK?</a>
-              <p></p>
-              <p>Mobile application  prototype for app that provides college-aged students with mental health resources in an easty-to-use, accessible manner</p>
-              <p>Completed for Special Topics in HCI course, where we were tasked with creating a home-healthcare technology UI prototype</p>
-              <p>Completed user research to outline usability goals of application, performed evaluation on initial prototype, created final prototype using figma</p>
-            </div>
             </a>
+            <div class="content">
+              <p>Mobile application framework prototype for app that provides college-aged students with mental health resources in an easty-to-use, accessible manner</p>
+              <p>Completed user research to outline usability goals of application, performed evaluation on initial prototype, created final prototype using figma</p>
+              <p>Final project for Special Topics in HCI course, where we were tasked with creating a home-healthcare technology UI prototype</p>
+            </div>
+            
           </div>
         </div>
       );
